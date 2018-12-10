1db61d785a3e938ffdbc32a436b22d32cf7fc7eb
Pior Bastida <pior@pbastida.net>
2018-12-09 20:22:59 -0500

    Add the formulae for DevBuddy

---
+++ devbuddy.rb
@@ -0,0 +1,29 @@
+class Devbuddy < Formula
+  desc "Setup your projects in a consistent way. Help other devs onboard!"
+  homepage "https://github.com/devbuddy/devbuddy"
+  url "https://github.com/devbuddy/devbuddy/archive/v0.7.0.tar.gz"
+  sha256 "7222ac7fcf08e8f877bc6a56e903a9b46aa6c639dd20efbdb47d2b4abca0d9b5"
+  head "https://github.com/devbuddy/devbuddy.git"
+
+  depends_on "go" => :build
+  depends_on "dep" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/devbuddy/devbuddy").install buildpath.children
+
+    cd "src/github.com/devbuddy/devbuddy" do
+      system "dep", "ensure", "-v", "-vendor-only"
+
+      ENV["DEVBUDDY_PLATFORMS"] = "darwin"
+      system "script/buildall"
+
+      bin.install "dist/bud-darwin-amd64" => "bud"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system bin/"bud", "--version"
+  end
+end
