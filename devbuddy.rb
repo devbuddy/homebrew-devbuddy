61fc93690b45b83b20cdaaae5b003982bedbc74f
Pior Bastida <pior@pbastida.net>
2022-02-11 10:10:40 -0400

    Update the recipe with new api

--- devbuddy.rb
+++ devbuddy.rb
@@ -1,26 +1,20 @@
 class Devbuddy < Formula
   desc "Setup your projects in a consistent way. Help other devs onboard!"
   homepage "https://github.com/devbuddy/devbuddy"
-  url "https://github.com/devbuddy/devbuddy.git", :tag => "v0.7.0"
+  url "https://github.com/devbuddy/devbuddy.git", :tag => "v0.12.0"
   sha256 "7222ac7fcf08e8f877bc6a56e903a9b46aa6c639dd20efbdb47d2b4abca0d9b5"
+  license "MIT"
   head "https://github.com/devbuddy/devbuddy.git"
 
   depends_on "go" => :build
-  depends_on "dep" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/devbuddy/devbuddy").install buildpath.children
+    versionString = Utils.popen_read("git describe --tags --dirty --always").chomp + "-homebrew"
+    ldflags = "-s -w -X main.Version=#{versionString}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "-o", "bud", "./cmd/bud"
 
-    cd "src/github.com/devbuddy/devbuddy" do
-      system "dep", "ensure", "-v", "-vendor-only"
-
-      ENV["DEVBUDDY_PLATFORMS"] = "darwin"
-      system "script/buildall"
-
-      bin.install "dist/bud-darwin-amd64" => "bud"
-      prefix.install_metafiles
-    end
+    bin.install "bud"
+    prefix.install_metafiles
   end
 
   test do
