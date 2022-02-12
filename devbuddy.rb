62229705f08fc0e54726208cd1a22e18edb6f1f1
Pior Bastida <pior@pbastida.net>
2022-02-12 16:49:06 -0400

    Secure the formulae with git revision rather than sha256

--- devbuddy.rb
+++ devbuddy.rb
@@ -1,10 +1,12 @@
 class Devbuddy < Formula
   desc "Setup your projects in a consistent way. Help other devs onboard!"
   homepage "https://github.com/devbuddy/devbuddy"
-  url "https://github.com/devbuddy/devbuddy.git", :tag => "v0.12.0"
-  sha256 "7222ac7fcf08e8f877bc6a56e903a9b46aa6c639dd20efbdb47d2b4abca0d9b5"
   license "MIT"
-  head "https://github.com/devbuddy/devbuddy.git"
+
+  url "https://github.com/devbuddy/devbuddy.git",
+      tag:      "v0.12.0",
+      revision: "f5eb9450ac6a615b06611c214583eb516385f151"
+  head "https://github.com/devbuddy/devbuddy.git", branch: "master"
 
   depends_on "go" => :build
 
